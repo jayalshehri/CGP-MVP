2c66b913147ac22e92db53815ad1287599cbd615
jayalshehri <jayalshehri@gmail.com>
2026-09-07 01:15:02 +0300

    أضف تقييم CSCC إلى التنقل

--- components/AppShell.tsx
+++ components/AppShell.tsx
@@ -16,6 +16,7 @@ const navigation = [
   { href: "/reports", label: "التقارير", icon: "▥", team: true },
   { href: "/executive", label: "اللوحة التنفيذية", icon: "◈", team: true },
   { href: "/third-parties", label: "مخاطر الأطراف الخارجية", icon: "◇", team: true },
+  { href: "/assessments", label: "تقييم CSCC", icon: "◎", team: true },
   { href: "/users", label: "إدارة المستخدمين", icon: "♙", admin: true },
   { href: "/feedback", label: "نتائج الاختبارات", icon: "✦", admin: true },
 ];
@@ -31,6 +32,7 @@ function NavIcon({ href }: { href: string }) {
     "/reports": "M4 3v18h17 M8 17v-5 M13 17V8 M18 17V5",
     "/executive": "M3 4h18v13H3z M8 21h8 M12 17v4 M7 13l4-4 3 2 3-4",
     "/third-parties": "M12 3l8 4v5c0 5-3.4 8-8 10-4.6-2-8-5-8-10V7z M9 12l2 2 4-4",
+    "/assessments": "M4 19V5 M4 19h16 M8 16v-4 M12 16V8 M16 16v-7 M20 16V5",
     "/users": "M8 12a4 4 0 1 0 0-8 4 4 0 0 0 0 8 M2 21v-2a6 6 0 0 1 12 0v2 M16 4a4 4 0 0 1 0 8 M17 15a5 5 0 0 1 5 5v1",
     "/feedback": "M20 11.5a8 8 0 0 1-8 8 8.8 8.8 0 0 1-3.4-.7L4 20l1.2-3.7A8 8 0 1 1 20 11.5z M8 11.5h.01 M12 11.5h.01 M16 11.5h.01",
   };
